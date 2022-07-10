38c297cc43644d25aebbdcbd6f900e978464211d
chirag gupta <chiragg1106@gmail.com>
2022-07-10 09:39:29 +0530

    content changes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -23,7 +23,7 @@
     <!-- Navigation-->
     <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
         <div class="container px-5">
-            <a class="navbar-brand" href="#">Dare2Coders</a>
+            <a class="navbar-brand" href="{{ url('/') }}">Dare2Coders</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ms-auto">
@@ -85,7 +85,8 @@
                 <div class="col-lg-7 order-lg-1">
                     <div class="p-lg-5">
                         <h3 class="display-5">Learn New <span class="text-danger display-5">Skills / Roadmaps</span></h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
+                        <p>“Learning is a treasure that will follow its owner everywhere.”</p>
+                        <p style="text-align: justify;">Learning new skills and getting a customized roadmap of the domain you want to master is always an advantage for new learners. The most beautiful thing about learning new skills is that no one can take them away from you. Skills are freedom, so get skilled today.</p>
                         <a class="btn btn-primary text-light btn-l rounded-pill" style="line-height: 1.6rem;" href="#">Read More &nbsp<i class="fa-solid fa-angle-right"></i></a>
                     </div>
                 </div>
@@ -102,7 +103,9 @@
                 <div class="col-lg-7">
                     <div class="p-lg-5">
                         <h2 class="display-5">Online Test <span class="text-danger">Training</span></h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
+                        <p>“Train your mind, to think outside the box.”</p>
+                        <p style="text-align: justify;">Training your mind enhances your mindset to tackle various situations in day-to-day activities.
+                        Opportunity to train and test your mind in an environment-friendly place which saves big-time and allows you to train at your own pace.</p>
                         <a class="btn btn-primary text-light btn-l rounded-pill" style="line-height: 1.6rem;" href="#">Read More &nbsp<i class="fa-solid fa-angle-right"></i></a>
                     </div>
                 </div>
@@ -119,7 +122,8 @@
                 <div class="col-lg-8 order-lg-1">
                     <div class="p-lg-5">
                         <h2 class="display-4">Hackathons<span class="text-danger"> & </span>Competitions</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
+                        <p>"There is no such thing as a long piece of work, except one that you dare not start."</p>
+                        <p style="text-align: justify;">Competitive programming, such as contests, challenges, and hackathons, can help open more doors in this area. The good news is that top-tier companies have turned into believers. So start tracking these coding challenges for new opportunities today, and you could be on your way to a new career.</p>
                         <a class="btn btn-primary text-light btn-l rounded-pill" style="line-height: 1.6rem;" href="#">Read More &nbsp<i class="fa-solid fa-angle-right"></i></a>
                     </div>
                 </div>
@@ -136,7 +140,8 @@
                 <div class="col-lg-8">
                     <div class="p-lg-5">
                         <h2 class="display-4">CV <span class="text-danger"> Modulator </span></h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
+                        <p>“Perfection is finally attained not when there is no longer anything to add, but when there is no longer anything to take away.”</p>
+                        <p style="text-align: justify;">Create a professional resume/cv that makes you stand out in the list of hundreds by using the templates. The ultimate tool to make a sleek resume that grows with your career.</p>
                         <a class="btn btn-primary text-light btn-l rounded-pill" style="line-height: 1.6rem;" href="#">Read More &nbsp<i class="fa-solid fa-angle-right"></i></a>
                     </div>
                 </div>
@@ -144,12 +149,16 @@
         </div>
     </section>
     <!-- Footer-->
-    <footer class="py-5 bg-black mt-3">
+    <footer class="py-4 bg-dark mt-5 main-footer">
         <div class="container px-5">
-            <p class="m-0 text-center text-white ">Copyright &copy; <script></script> Dare2Coders</p>
-            <p class="m-0 text-center text-white ">Designed and Developed by <span class="text-primary"><a href="https://www.incredibals.dare2coders.com" class="text-decoration-none">Incredibals</a></span></p>
+            <p class="m-0 text-center text-light ">Copyright &copy; <script>
+                    var year = new Date();
+                    document.write(year.getFullYear());
+                </script> <a href="https://www.dare2coders.com" class="text-primary text-decoration-none">Dare2Coders</a>. &nbsp; All Right Reserved.</p>
+            <p class="m-0 text-center text-light"> Designed and Developed by <span class="text-primary"><a href="https://incredibles.dare2coders.com" class="text-decoration-none">Incredibals</a>.</span></p>
         </div>
     </footer>
+
     <!-- Bootstrap core JS-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <!-- Core theme JS-->
