362166d58d3cd76f56204a8f47efc34742844659
chirag gupta <chiragg1106@gmail.com>
2022-07-19 21:46:04 +0530

    major changes

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit9e43e1381ff51067ccf237a6e94acd74
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit9e43e1381ff51067ccf237a6e94acd74', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit9e43e1381ff51067ccf237a6e94acd74', 'loadClassLoader'));
